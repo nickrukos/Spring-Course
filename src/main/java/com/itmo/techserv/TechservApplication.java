0bcbe6c4c9d239aca6ccd8c0c4f9b7213a8b6431
nickrukos <nickrukos@mail.ru>
2024-11-27 15:01:07 +0300

    Домашнее задание к уроку 3

---
+++ src/main/java/com/itmo/techserv/TechservApplication.java
@@ -0,0 +1,13 @@
+package com.itmo.techserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechservApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechservApplication.class, args);
+    }
+
+}
