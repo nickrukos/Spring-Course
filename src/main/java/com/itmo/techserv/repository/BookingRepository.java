d431d0942f29bb608ed9ce2360f8841b52769cf6
nickrukos <nickrukos@mail.ru>
2024-12-11 00:09:52 +0300

    Домашнее задание к урокам 3-7

--- src/main/java/com/itmo/techserv/repository/BookingRepository.java
+++ src/main/java/com/itmo/techserv/repository/BookingRepository.java
@@ -1,7 +1,27 @@
 package com.itmo.techserv.repository;
 
 import com.itmo.techserv.entity.Booking;
+import com.itmo.techserv.entity.TechService;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+@Repository
 public interface BookingRepository extends JpaRepository<Booking,Long> {
+    //получение списка броней
+    List<Booking> findAllByLogin(String login);
+    //получение списка представленных услуг
+    @Query(nativeQuery = true, value = "SELECT techservice.* FROM booking INNER JOIN techservice"
+                                       + "ON techservice.id = booking.category_id "
+                                       + "WHERE booking.login = :login")
+    List<TechService> findServices(String login);
+    //получение списка представленных услуг
+    List<TechService> findServiceByLogin(String login);
+
+    //создание брони на услугу сервиса
+    void Save(Booking booking);
+
+    //редактирование брони
+    //Booking updateBookingBy
 }
--- src/main/java/com/itmo/techserv/repository/ServiceRepository.java
+++ src/main/java/com/itmo/techserv/repository/ServiceRepository.java
@@ -2,7 +2,9 @@ package com.itmo.techserv.repository;
 
 import com.itmo.techserv.entity.TechService;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ServiceRepository  extends JpaRepository<TechService,Long> {
 
 }
